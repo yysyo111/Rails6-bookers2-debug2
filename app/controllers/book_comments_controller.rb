caa33c3756ec9b782d096748413b1b47647f2807
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-05-10 11:43:12 +0000

    [Add]コメント機能追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -8,6 +8,11 @@ class BookCommentsController < ApplicationController
     redirect_to request.referer 
   end
   
+  def destroy 
+    BookComment.find(params[:id]).destroy
+    redirect_to request.referer
+  end
+  
   private 
   
   def book_comment_params 

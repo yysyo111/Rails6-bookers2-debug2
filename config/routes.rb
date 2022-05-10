6d50046c1abd04381decb11bf8e6a413ea1b3083
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-05-10 09:00:37 +0000

    [Add]いいね機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,14 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+  end
+  
+  
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# いいね機能の場合は「1人のユーザーは1つの投稿に対して1回しかいいねできない」という仕様
+# resourceは「それ自身のidが分からなくても、関連する他のモデルのidから特定できる」
